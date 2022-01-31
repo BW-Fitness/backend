5f77abe027cdc6796235633e5b86f3c3e838da53
Claire List <claire.list2501@gmail.com>
2022-01-31 16:03:01 -0700

    SEARCHFOR tests written

--- _tests_/client.test.js
+++ _tests_/client.test.js
@@ -41,15 +41,45 @@ describe('client',()=>{
         const res = await request(server).get('/api/classes');
         expect(res.body).toMatch({mockedGet}); //will have to see if this actually works
     });
+
+    it('can SEARCHFOR classes by DATE', async () => {
+        const res = await request(server).get('/api/classes/:query').send({date: '12/31/1969'});
+        expect(res.body.message).toMatch('Classes on 12/31/1969:');
     });
 
+    it('can SEARCHFOR classes by TIME', async () => {
+        const res = await request(server).get('/api/classes/:query').send({time: '4:20'});
+        expect(res.body.message).toMatch('Classes at 4:20:');
+    });
+
+    it('can SEARCHFOR classes by DURATION', async () => {
+        const res = await request(server).get('/api/classes/:query').send({duration: '4:20'});
+        expect(res.body.message).toMatch('Classes lasting 4:20:');
+    });
+
+    it('can SEARCHFOR classes by TYPE', async () => {
+        const res = await request(server).get('/api/classes/:query').send({type: 'calisthenics'});
+        expect(res.body.message).toMatch('calisthenics classes:');
+    });
+
+    it('can SEARCHFOR classes by INTENSITY', async () => {
+        const res = await request(server).get('/api/classes/:query').send({intensity: 'beginner'});
+        expect(res.body.message).toMatch('beginner classes:');
+    });
+
+    it('can SEARCHFOR classes by LOCATION', async () => {
+        const res = await request(server).get('/api/classes/:query').send({location: 'local'});
+        expect(res.body.message).toMatch('classes near local:');
+    });
+});
+
     //TODOS ::
-//     -authenticated client can SEARCHFOR classes by date 
-// -authenticated client can SEARCHFOR classes by time
-// -authenticated client can SEARCHFOR classes by duration
-// -authenticated client can SEARCHFOR classes by type
-// -authenticated client can SEARCHFOR classes by intensity
-// -authenticated client can SEARCHFOR classes by location
+//     -authenticated client can SEARCHFOR classes by date  X
+// -authenticated client can SEARCHFOR classes by time X
+// -authenticated client can SEARCHFOR classes by duration X
+// -authenticated client can SEARCHFOR classes by type X
+// -authenticated client can SEARCHFOR classes by intensity X
+// -authenticated client can SEARCHFOR classes by location X
 
 // -authenticated client can reserve a spot in a class with available open seats, returns expected class size (-1)
 // -authenticated client can cancel a reservation, returns expected class size (+1)
