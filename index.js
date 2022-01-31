5f616d97439f2227c532908d83154abd812a2f19
Claire List <claire.list2501@gmail.com>
2022-01-31 13:16:47 -0700

    remember to git ignore node modules

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //the index of the project!
+require('dotenv').config();
+const server = require('./api/server.js');
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
