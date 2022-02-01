f69312d507606f542c0ca6533bf1919c1036e898
jcpcabanada <jcpcabanada@gmail.com>
2022-01-31 19:43:49 -0800

    MIGRATION file made! and seed

---
+++ data/migrations/20220201033633_todo.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users.string('username', 255).notNullable().unique();
+        users.string('password', 255).notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
+};
