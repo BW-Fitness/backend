7b7430b5d72fb141f47b023b5afc4b6b911d0851
jcpcabanada <jcpcabanada@gmail.com>
2022-02-01 15:17:11 -0800

    3 tables still need to add columns to instructors table and clients table

--- data/migrations/20220201033633_todo.js
+++ data/migrations/20220201033633_todo.js
@@ -6,18 +6,31 @@ exports.up = function (knex) {
     return knex.schema
         .createTable('classes', table => {
             table.increments('class_id');
+            table.string('class_id').notNullable()
             table.string('name', 100).notNullable();
             table.string('type', 100).notNullable();
             table.date('start time').notNullable();
             table.time('duration').notNullable()
             table.string('intensity level', 100).notNullable();
             table.string('location', 100).notNullable();
+            table.string('current # of registered attendees', 1000).notNullable()
+            table.string('max class size', 1000).notNullable()
 
+        })
+        .createTable('instructors', table => {
+            table.increments('instructor_id');
+            table.string('instructor_id').notNullable()
 
+        })
+        .createTable('clients', table => {
+            table.increments('client_id');
+            table.string('client_id').notNullable()
 
-        });
+        })
 };
 
 exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('clients');
+    return knex.schema.dropTableIfExists('instructors');
     return knex.schema.dropTableIfExists('classes');
 };
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server');
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
-    console.log(` ===> Server Listening on port ${PORT} <===`);
+    console.log(` ===> Server Listening on port htt${PORT} <===`);
 })
