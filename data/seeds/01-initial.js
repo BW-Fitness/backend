7b7e6c89091dea4d5b2e45a5a84f52bfb84334c0
jcpcabanada <jcpcabanada@gmail.com>
2022-02-03 09:41:12 -0800

    added seeds and new database

--- data/seeds/01-initial.js
+++ data/seeds/01-initial.js
@@ -1,16 +1,8 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
+const { clean } = require('knex-cleaner')
+
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return clean(knex, {
+      mode: 'truncate',
+      ignoreTables: [ 'knex_migrations', 'knex_migrations_lock' ]
+  })
 };
