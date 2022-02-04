e61184f8efa1207ef10bb389b22f0b2641fa96ae
Leo Oladimu <leooladimu9@gmail.com>
2022-02-04 01:16:13 -0500

    put all original db back

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //the index of the project!
 require('dotenv').config();
 const server = require('./api/server.js');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1619;
 
 server.listen(PORT, () => {
     console.log(` ===> Server Listening on port http://localhost:${PORT} <===`);
